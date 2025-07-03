77cc7941b2501e54992933ef90a1686d8273fe40
CodePsy-2001 <codepsy2001@gmail.com>
2025-07-03 15:46:59 +0900

    feat: 개혁신당 CI 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,20 @@
 import type { Metadata } from 'next'
 import './global.css'
 
+const title = '개혁신당 국민연금 계산기'
+const description = '구/신연금 분리하면 어떻게 될까요?'
+
 export const metadata: Metadata = {
-  title: 'NextJS Lite Boilerplate',
-  description: 'Only includes what you actually need:',
+  title,
+  description,
   appleWebApp: {
     capable: true,
     title: 'NextJS Lite',
     statusBarStyle: 'default',
   },
   openGraph: {
-    title: 'NextJS Lite Boilerplate',
-    description: 'Only includes what you actually need:',
+    title,
+    description,
     images: [{
       url: '/og.png',
       width: 1200,
@@ -21,8 +24,8 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'NextJS Lite Boilerplate',
-    description: 'Only includes what you actually need:',
+    title,
+    description,
     images: ['/og.png'],
   },
 }
--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,21 +2,22 @@ import type { MetadataRoute } from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'NextJS Lite Boilerplate',
-    short_name: 'NextJS Lite',
+    name: '개혁신당 국민연금 계산기',
+    short_name: '연금계산기',
     start_url: '/',
     display: 'standalone',
-    background_color: '#fff',
-    theme_color: '#fff',
+    background_color: '#FFFFFF',
+    theme_color: '#FF7210',
     icons: [
-      {
-        src: '/favicon.ico',
-        sizes: 'any',
-        type: 'image/x-icon',
-      },
       {
         src: '/web-app-manifest-192x192.png',
         sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: '/web-app-manifest-512x512.png',
+        sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable',
       },
